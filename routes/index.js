8ea881fe1ae287047dfba0397a4855deee7bcf06
tanjiahui <673800357@qq.com>
2017-10-06 13:16:14 +0800

    demo

---
+++ routes/index.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+var multer  = require('multer')
+var storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, './uploads')
+  },
+  filename: function (req, file, cb) {
+    cb(null, file.fieldname + '-' + Date.now()+'.jpg')
+  }
+})
+var upload = multer({ storage: storage })
+var path = require('path');
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(__dirname)
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+router.post('/upload',upload.any(), (req,res) =>{
+  res.end('ok')
+})
+module.exports = router;
